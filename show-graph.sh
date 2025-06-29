a42c42ab743f988832f77af31624868cc7a2d61c
Migacz85 <migac85@gmail.com>
2025-06-29 16:22:55 +0100

    refactor: Convert project to pure Python implementation and remove power management scripts

--- show-graph.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-source $SCRIPTPATH/charts/bin/activate
-python chart.py &
-deactivate
-read
-
--- update-req.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-source charts/bin/activate
-pip freeze > requirements.txt
-echo "Requirements updated"
-deactivate
